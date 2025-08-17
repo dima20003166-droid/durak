fa30170188f93096601eea6616b4c0eac1ccd960
dima20003166-droid <dima20003166@gmail.com>
2025-08-17 07:22:08 +0300

    1

--- durak-main/client/src/utils/parallax.js
+++
@@ -1,7 +0,0 @@
-// client/src/utils/parallax.js
-export function updateParallax(x, y) {
-  if (typeof document === 'undefined') return;
-  const root = document.documentElement;
-  root.style.setProperty('--parallax-x', `${x}px`);
-  root.style.setProperty('--parallax-y', `${y}px`);
-}
