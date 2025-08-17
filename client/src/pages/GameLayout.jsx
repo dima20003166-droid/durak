60d94462bc13a3dbd123dc79f7dfc917c7318865
dima20003166-droid <dima20003166@gmail.com>
2025-08-17 14:08:55 +0300

    345345

--- client/src/pages/GameLayout.jsx
+++ client/src/pages/GameLayout.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const GameLayout = ({ header, table, footer }) => (
+const GameLayout = ({
+  header,
+  table,
+  players,
+  leftSidebar,
+  rightSidebar,
+  footer,
+}) => (
   <div className="game-layout text-text game-bg">
     {header && <header className="game-header">{header}</header>}
-    {table && (
-      <div className="game-table">
-        <div className="table-oval">
-          <div className="table-slots">{table}</div>
-        </div>
+    <div className="game-main justify-center">
+      {leftSidebar ? <aside className="game-left">{leftSidebar}</aside> : (rightSidebar ? <aside className="game-left" aria-hidden="true" /> : null)}
+      <div className="game-center">
+        {table && <div className="game-table">{table}</div>}
+        {players && <div className="game-players">{players}</div>}
       </div>
-    )}
+      {rightSidebar && <aside className="game-right">{rightSidebar}</aside>}
+    </div>
     {footer && <footer className="game-footer">{footer}</footer>}
   </div>
 );
