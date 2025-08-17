584846b3f221382f40a4bd17ef4db0615bd421c0
dima20003166-droid <dima20003166@gmail.com>
2025-08-17 15:49:05 +0300

    34534

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,3 +1,3 @@
-import axios from 'axios';
-const http = axios.create({ baseURL: '/api', withCredentials: true });
-export default http;
+import axios from 'axios'
+const http = axios.create({ baseURL: '/api', withCredentials: true })
+export default http
